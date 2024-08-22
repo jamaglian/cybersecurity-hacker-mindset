ed76231134a518cff446477ccea1a2a9142c5714
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-22 20:52:56 -0300

    EX1

---
+++ EX1/README.md
@@ -0,0 +1,2 @@
+# EX1
+Criar um script em bash que crie uma estrutura de diretórios começando pelo seu nome, nome do curso, seguido das matérias, com arquivos dentro de cada pasta e algum texto dentro do arquivo.
---
+++ EX1/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p ./joao/defesa_cibernetica/{hacker_mindset,computer_architecture,hardware_hacking,network_security,security_management,linux,redes,windows}
+echo "Anotações" > ./joao/defesa_cibernetica/hacker_mindset/notas.txt 
+echo "Anotações" > ./joao/defesa_cibernetica/computer_architecture/notas.txt 
+echo "Anotações" > ./joao/defesa_cibernetica/hardware_hacking/notas.txt 
+echo "Anotações" > ./joao/defesa_cibernetica/network_security/notas.txt 
+echo "Anotações" > ./joao/defesa_cibernetica/security_management/notas.txt 
+echo "Anotações" > ./joao/defesa_cibernetica/linux/notas.txt 
+echo "Anotações" > ./joao/defesa_cibernetica/redes/notas.txt 
+echo "Anotações" > ./joao/defesa_cibernetica/windows/notas.txt 
