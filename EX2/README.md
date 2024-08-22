cd00a518918e89796042c7d424707f3ddc9d92b7
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-22 20:58:04 -0300

    EX2

---
+++ EX2/README.md
@@ -0,0 +1,2 @@
+# EX1
+Criar um script em bash que exiba no terminal informações do sistema como, usuário logado, nome do host, sistema operacional usado e path atual de onde o script está sendo executado.
---
+++ EX2/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "============= AULA 04 ============="
+echo "usuario :" $(whoami)
+echo "hostname: " $(hostname)
+echo " SO: " $(uname)
+echo " Path:" $(pwd)
+echo "============ BASH SCRIPT =========="
